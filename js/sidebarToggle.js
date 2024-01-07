8ff61dc861609254f5f11c13b58fa08733393e9d
vigs950813 <renovatio_bluesunset@hotmail.com>
2024-01-07 12:45:03 -0600

    prueba

--- js/sidebarToggle.js
+++ js/sidebarToggle.js
@@ -12,12 +12,12 @@ window.addEventListener('DOMContentLoaded', event => {
     // Toggle the side navigation
     const sidebarToggle = document.body.querySelector('#sidebarToggle');
     if (sidebarToggle) {
-        alert("saludos");
         // Uncomment Below to persist sidebar toggle between refreshes
         // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
         //     document.body.classList.toggle('sb-sidenav-toggled');
         // }
         sidebarToggle.addEventListener('click', event => {
+            alert("saludos");
             event.preventDefault();
             document.body.classList.toggle('sb-sidenav-toggled');
             localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
