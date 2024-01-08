c0417a141d122d3c62049a0a06996504508c0c5d
vigs950813 <renovatio_bluesunset@hotmail.com>
2024-01-08 16:39:59 -0600

    prueba

---
+++ js/headerLoaderPages.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const headerContainer = document.getElementById('header-container');
+
+    fetch('headerLoader.html') // Ruta al archivo header.html
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(data => {
+            headerContainer.innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Hubo un problema al cargar el header:', error);
+        });
+});
