1e0c620304d89acc820a9d99af60686515e6cfb0
vigs950813 <renovatio_bluesunset@hotmail.com>
2024-01-08 17:02:52 -0600

    prueba

---
+++ js/footerLoaderPages.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const footerContainer = document.getElementById('footer-container');
+
+    fetch('../dynamic/footer.html') // Ruta al archivo footer.html
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(data => {
+            footerContainer.innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Hubo un problema al cargar el footer:', error);
+        });
+});
---
+++ js/navbarLoaderPages.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const navbarIndexContainer = document.getElementById('navbarIndex-container');
+
+    fetch('../dynamic/navbarIndex.html') // Ruta al archivo header.html
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(data => {
+            navbarIndexContainer.innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Hubo un problema al cargar el navbar del index:', error);
+        });
+});
